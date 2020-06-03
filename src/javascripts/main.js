b85479f107a5dcecf281fd4b484330c30970f7f3
NateOwens <nathanieltylerowens@gmail.com>
2020-06-02 21:12:44 -0500

    got shit working with a hint of Sass

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,10 @@
+import studentList from './components/studentList/studentList';
 import '../styles/main.scss';
+
+const init = () => {
+  studentList.createStudentList();
+};
+
+init();
 
 console.error("you're a wizard harry");
